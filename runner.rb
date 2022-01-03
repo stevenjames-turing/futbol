e10087d7784d69e56ade4ce9d64910a2acab9b53
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-03 17:02:39 -0600

    Add runner.rb

---
+++ runner.rb
@@ -0,0 +1,15 @@
+require './lib/stat_tracker'
+
+game_path = './data/games.csv'
+team_path = './data/teams.csv'
+game_teams_path = './data/game_teams.csv'
+
+locations = {
+  games: game_path,
+  teams: team_path,
+  game_teams: game_teams_path
+}
+
+stat_tracker = StatTracker.from_csv(locations)
+
+require 'pry'; binding.pry
