3cab524b2c1ca183c5070a894a2a85cb247b6882
Tyler Ross <tross020890@gmail.com>
2022-01-04 03:32:51 -0600

    Feat: add stat tracker, passes exists test

---
+++ lib/stat_tracker.rb
@@ -0,0 +1,20 @@
+#files sent to from_csv from runner. from_csv creates new object with hash containing file locations
+require "./lib/game_statistics.rb"
+include GameStatistics
+class StatTracker
+
+
+  def initialize(paths)
+    @paths = paths
+    
+  end
+
+  def self.from_csv(location)
+    stat_tracker = StatTracker.new(location)
+  end
+
+  #calls method in game stats, passes games location to be accessed
+  def highest_total_score
+    GameStatistics.highest_total_score(@paths[:games])
+  end
+end
---
+++ spec/stat_tracker_spec.rb
@@ -0,0 +1,25 @@
+require './lib/stat_tracker.rb'
+
+RSpec.describe StatTracker do
+  before(:all) do
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
+
+  it "exists" do
+    expect(@stat_tracker).to be_an_instance_of StatTracker
+  end
+
+  #xit "#highest_total_score" do
+#    expect(@stat_tracker.highest_total_score).to eq 11
+#  end
+end
