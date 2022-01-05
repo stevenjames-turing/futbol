86817461d6623808c571cf81d66415922f404558
Alicia Watt <alicia.watt.aw@gmail.com>
2022-01-05 16:33:36 -0700

    test: winningest coach method

---
+++ spec/season_stats_spec.rb
@@ -0,0 +1,24 @@
+require "./lib/stat_tracker.rb"
+
+RSpec.describe SeasonStats do
+  before(:all) do
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
+
+   describe 'SeasonStats module methods' do
+     describe '#winningest_coach' do
+       it 'returns the name of the Coach with the best win percentage for the season'
+       expect(stat_tracker.winningest_coach()).to eq()
+      end
+    end
+  end
