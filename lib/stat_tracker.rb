019a1f2c4d34eeffa4ea815f6cf775e299b5e7ff
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-06 16:42:52 -0600

    Remove methods from StatTracker class and call from LeagueStats module

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,52 +1,25 @@
 #files sent to from_csv from runner. from_csv creates new object with hash containing file locations
 require "./lib/game_statistics.rb"
 require './lib/league_stats'
+require './lib/stats_data'
 include GameStatistics
-include LeagueStats
 class StatTracker
+  include LeagueStats
+  include StatsData
 
 
-  def initialize(file_paths)
-    @games_data = file_paths[:games]
-    @teams_data = file_paths[:teams]
-    @game_teams_data = file_paths[:game_teams]
+  # def initialize(file_paths)
+  #   @games_data = file_paths[:games]
+  #   @teams_data = file_paths[:teams]
+  #   @game_teams_data = file_paths[:game_teams]
+  # end
 
-  end
-
-  def self.from_csv(location)
-    stat_tracker = StatTracker.new(location)
+  def self.from_csv(locations)
+    stat_tracker = StatTracker.new(locations)
   end
 
   #calls method in game stats, passes games location to be accessed
   def highest_total_score
     GameStatistics.highest_total_score(@games_data)
-  end
-
-  def count_of_teams
-    LeagueStats.count_of_teams(@teams_data)
-  end
-
-  def best_offense
-    LeagueStats.best_offense(@games_data, @teams_data)
-  end
-
-  def worst_offense
-    LeagueStats.worst_offense(@games_data, @teams_data)
-  end
-
-  def highest_scoring_visitor
-    LeagueStats.highest_scoring_visitor(@games_data, @teams_data)
-  end
-
-  def highest_scoring_home_team
-    LeagueStats.highest_scoring_home_team(@games_data, @teams_data)
-  end
-
-  def lowest_scoring_visitor
-    LeagueStats.lowest_scoring_visitor(@games_data, @teams_data)
-  end
-
-  def lowest_scoring_home_team
-    LeagueStats.lowest_scoring_home_team(@games_data, @teams_data)
   end
 end
