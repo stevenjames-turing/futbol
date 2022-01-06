ebe39db65518aa50efef8493e48ee837e9744ef1
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-05 19:39:04 -0600

    Add LeagueStats module and #count_of_teams

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -1,10 +1,12 @@
 require 'csv'
+module LeagueStats
 
-class LeagueStats
-
-  def initialize
+  def count_of_teams(data)
+    contents = CSV.open data, headers: true, header_converters: :symbol
+    teams = contents.map do |row|
+      :team_id
+    end.count
   end
 
-  
 
 end
