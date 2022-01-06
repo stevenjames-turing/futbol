3d96f0e69a48075e48a7b59e93685bf271808b06
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-06 16:41:50 -0600

    Refactor for CVS files to open during initialize vs each method

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -1,19 +1,16 @@
 require 'csv'
 module LeagueStats
 
-  def count_of_teams(data)
-    contents = CSV.open data, headers: true, header_converters: :symbol
-    teams = contents.map do |row|
+  def count_of_teams
+    teams = @teams_data.map do |row|
       :team_id
     end.count
   end
 
-  def goal_data_by_team(games_data, teams_data)
-    teams_data = CSV.open teams_data, headers: true, header_converters: :symbol
-    games_data = CSV.open games_data, headers: true, header_converters: :symbol
+  def goal_data_by_team
     goals_per_team = {}
     # gets all goal data from games.csv
-    games_data.each do |row|
+    @games_data.each do |row|
       if !goals_per_team.has_key?(row[:away_team_id])
         goals_per_team[row[:away_team_id]] = {team_name: nil,
           goals_while_away: row[:away_goals].to_i,
@@ -25,42 +22,42 @@ module LeagueStats
           avg_goals_per_game_away: 0,
           avg_goals_per_game_home: 0,
           avg_goals_per_game_total: 0}
-      elsif goals_per_team.has_key?(row[:away_team_id])
-        (goals_per_team[row[:away_team_id]][:goals_while_away] += row[:away_goals].to_i) && (goals_per_team[row[:away_team_id]][:total_games_played] += 1) &&
-        (goals_per_team[row[:away_team_id]][:total_goals] += row[:away_goals].to_i) && (goals_per_team[row[:away_team_id]][:away_games_played] += 1)
-      end
-      if !goals_per_team.has_key?(row[:home_team_id])
-        goals_per_team[row[:home_team_id]] = {team_name: nil,
-          goals_while_away: 0,
-          goals_while_home: row[:home_goals].to_i,
-          total_goals: row[:home_goals].to_i,
-          away_games_played: 0,
-          home_games_played: 1,
-          total_games_played: 1,
-          avg_goals_per_game_away: 0,
-          avg_goals_per_game_home: 0,
-          avg_goals_per_game_total: 0}
-      elsif goals_per_team.has_key?(row[:home_team_id])
-        (goals_per_team[row[:home_team_id]][:goals_while_home] += row[:home_goals].to_i) && (goals_per_team[row[:home_team_id]][:total_games_played] += 1) &&
-        (goals_per_team[row[:home_team_id]][:total_goals] += row[:home_goals].to_i) && (goals_per_team[row[:home_team_id]][:home_games_played] += 1)
-      end
-    end
-    # calculates averages and assigns team names
-    teams_data.each do |row|
-      goals_per_team.each_pair do |team, stats|
-        row[:team_id] == team ? stats[:team_name] = row[:teamname] : next
-        stats[:avg_goals_per_game_total] = stats[:total_goals].fdiv(stats[:total_games_played])
-        stats[:avg_goals_per_game_away] = stats[:goals_while_away].fdiv(stats[:away_games_played])
-        stats[:avg_goals_per_game_home] = stats[:goals_while_home].fdiv(stats[:home_games_played])
-      end
-    end
-    goals_per_team
+        elsif goals_per_team.has_key?(row[:away_team_id])
+          (goals_per_team[row[:away_team_id]][:goals_while_away] += row[:away_goals].to_i) && (goals_per_team[row[:away_team_id]][:total_games_played] += 1) &&
+          (goals_per_team[row[:away_team_id]][:total_goals] += row[:away_goals].to_i) && (goals_per_team[row[:away_team_id]][:away_games_played] += 1)
+        end
+        if !goals_per_team.has_key?(row[:home_team_id])
+          goals_per_team[row[:home_team_id]] = {team_name: nil,
+            goals_while_away: 0,
+            goals_while_home: row[:home_goals].to_i,
+            total_goals: row[:home_goals].to_i,
+            away_games_played: 0,
+            home_games_played: 1,
+            total_games_played: 1,
+            avg_goals_per_game_away: 0,
+            avg_goals_per_game_home: 0,
+            avg_goals_per_game_total: 0}
+          elsif goals_per_team.has_key?(row[:home_team_id])
+            (goals_per_team[row[:home_team_id]][:goals_while_home] += row[:home_goals].to_i) && (goals_per_team[row[:home_team_id]][:total_games_played] += 1) &&
+            (goals_per_team[row[:home_team_id]][:total_goals] += row[:home_goals].to_i) && (goals_per_team[row[:home_team_id]][:home_games_played] += 1)
+          end
+        end
+        # calculates averages and assigns team names
+        @teams_data.each do |row|
+          goals_per_team.each_pair do |team, stats|
+            row[:team_id] == team ? stats[:team_name] = row[:teamname] : next
+            stats[:avg_goals_per_game_total] = stats[:total_goals].fdiv(stats[:total_games_played])
+            stats[:avg_goals_per_game_away] = stats[:goals_while_away].fdiv(stats[:away_games_played])
+            stats[:avg_goals_per_game_home] = stats[:goals_while_home].fdiv(stats[:home_games_played])
+          end
+        end
+        goals_per_team
   end
 
-  def best_offense(games_data, teams_data)
+  def best_offense
     best_offense = nil
     highest_average = 0
-    goal_data_by_team(games_data, teams_data).each_pair do |team, data|
+    goal_data_by_team.each_pair do |team, data|
       if data[:avg_goals_per_game_total] >= highest_average
         highest_average = data[:avg_goals_per_game_total]
         (best_offense = data[:team_name])
@@ -69,10 +66,10 @@ module LeagueStats
     best_offense
   end
 
-  def worst_offense(games_data, teams_data)
+  def worst_offense
     worst_offense = nil
     lowest_average = 100
-    goal_data_by_team(games_data, teams_data).each_pair do |team, data|
+    goal_data_by_team.each_pair do |team, data|
       if data[:avg_goals_per_game_total] < lowest_average
         lowest_average = data[:avg_goals_per_game_total]
         (worst_offense = data[:team_name])
@@ -81,10 +78,10 @@ module LeagueStats
     worst_offense
   end
 
-  def highest_scoring_visitor(games_data, teams_data)
+  def highest_scoring_visitor
     highest_away = 0
     highest_scoring_visitor = nil
-    goal_data_by_team(games_data, teams_data).each_pair do |team, data|
+    goal_data_by_team.each_pair do |team, data|
       if data[:avg_goals_per_game_away] > highest_away
         highest_away = data[:avg_goals_per_game_away]
         highest_scoring_visitor = data[:team_name]
@@ -93,10 +90,10 @@ module LeagueStats
     highest_scoring_visitor
   end
 
-  def highest_scoring_home_team(games_data, teams_data)
+  def highest_scoring_home_team
     highest_home = 0
     highest_scoring_home = nil
-    goal_data_by_team(games_data, teams_data).each_pair do |team, data|
+    goal_data_by_team.each_pair do |team, data|
       if data[:avg_goals_per_game_home] > highest_home
         highest_home = data[:avg_goals_per_game_home]
         highest_scoring_home = data[:team_name]
@@ -105,10 +102,10 @@ module LeagueStats
     highest_scoring_home
   end
 
-  def lowest_scoring_visitor(games_data, teams_data)
+  def lowest_scoring_visitor
     lowest_away = 100
     lowest_scoring_visitor = nil
-    goal_data_by_team(games_data, teams_data).each_pair do |team, data|
+    goal_data_by_team.each_pair do |team, data|
       if data[:avg_goals_per_game_away] < lowest_away
         lowest_away = data[:avg_goals_per_game_away]
         lowest_scoring_visitor = data[:team_name]
@@ -117,10 +114,10 @@ module LeagueStats
     lowest_scoring_visitor
   end
 
-  def lowest_scoring_home_team(games_data, teams_data)
+  def lowest_scoring_home_team
     lowest_home = 100
     lowest_scoring_home = nil
-    goal_data_by_team(games_data, teams_data).each_pair do |team, data|
+    goal_data_by_team.each_pair do |team, data|
       if data[:avg_goals_per_game_home] < lowest_home
         lowest_home = data[:avg_goals_per_game_home]
         lowest_scoring_home = data[:team_name]
@@ -128,4 +125,18 @@ module LeagueStats
     end
     lowest_scoring_home
   end
+
+  # Method for TeamStats for Sam
+  # def team_info(team_id)
+  #   team_info_full = @teams_data.find do |row|
+  #     row[:team_id] == team_id
+  #   end
+  #   team_info = {
+  #     team_id: team_info_full[:team_id],
+  #     franchise_id: team_info_full[:franchiseid],
+  #     team_name: team_info_full[:teamname],
+  #     abbreviation: team_info_full[:abbreviation],
+  #     link: team_info_full[:link]}
+  #   team_info
+  # end
 end
