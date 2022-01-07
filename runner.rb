af2b15b8a8d98b2de806cebee3eeafdddf0b92e7
Sam Smith <samlsmith424@gmail.com>
2022-01-07 09:55:48 -0800

    Test: change files to test files

--- runner.rb
+++ runner.rb
@@ -1,8 +1,12 @@
 require './lib/stat_tracker'
 
-game_path = './data/games.csv'
-team_path = './data/teams.csv'
-game_teams_path = './data/game_teams.csv'
+# game_path = './data/games.csv'
+# team_path = './data/teams.csv'
+# game_teams_path = './data/game_teams.csv'
+
+game_path = './data/test_games.csv'
+team_path = './data/test_teams.csv'
+game_teams_path = './data/test_game_teams.csv'
 
 locations = {
   games: game_path,
