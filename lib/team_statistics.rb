2516da5f219143e01676955b87514eb1d0d82ecb
Sam Smith <samlsmith424@gmail.com>
2022-01-07 10:42:30 -0800

    Feat: winning team

--- lib/team_statistics.rb
+++ lib/team_statistics.rb
@@ -1,17 +1,25 @@
-require_relative './team'
+# require_relative './team'
+# require_relative './game'
+# require_relative './game_team'
 
 module TeamStatistics
 
   def team_info(team_id)
     team_info_full = @teams_data.find do |row|
-      row[:team_id].to_i == team_id
+      row[:team_id] == team_id
     end
     team_info = {
-      team_id: team_info_full[:team_id].to_i,
+      team_id: team_info_full[:team_id],
       franchise_id: team_info_full[:franchiseid],
       team_name: team_info_full[:teamname],
       abbreviation: team_info_full[:abbreviation],
       link: team_info_full[:link]}
     team_info
   end
+
+  def team_win?(team_id, game_id)
+    require "pry"; binding.pry
+    
+  end
+
 end
