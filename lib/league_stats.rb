c8a7939cd3eea9d6f6611031b5f3055b7203812b
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-07 21:41:31 -0600

    Refactor methods to work with objects instead of hashes

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -3,7 +3,7 @@ module LeagueStats
 
   def count_of_teams
     teams = @teams_data.map do |row|
-      :team_id
+      row.team_id
     end.count
   end
 
@@ -12,40 +12,40 @@ module LeagueStats
     # gets all goal data from games.csv
     @games_data.each do |row|
       if !goals_per_team.has_key?(row.away_team_id)
-        goals_per_team[row[:away_team_id]] = {team_name: nil,
-          goals_while_away: row[:away_goals].to_i,
+        goals_per_team[row.away_team_id] = {team_name: nil,
+          goals_while_away: row.away_goals.to_i,
           goals_while_home: 0,
-          total_goals: row[:away_goals].to_i,
+          total_goals: row.away_goals.to_i,
           away_games_played: 1,
           home_games_played: 0,
           total_games_played: 1,
           avg_goals_per_game_away: 0,
           avg_goals_per_game_home: 0,
           avg_goals_per_game_total: 0}
-      elsif goals_per_team.has_key?(row[:away_team_id])
-        (goals_per_team[row[:away_team_id]][:goals_while_away] += row[:away_goals].to_i) && (goals_per_team[row[:away_team_id]][:total_games_played] += 1) &&
-        (goals_per_team[row[:away_team_id]][:total_goals] += row[:away_goals].to_i) && (goals_per_team[row[:away_team_id]][:away_games_played] += 1)
+      elsif goals_per_team.has_key?(row.away_team_id)
+        (goals_per_team[row.away_team_id][:goals_while_away] += row.away_goals.to_i) && (goals_per_team[row.away_team_id][:total_games_played] += 1) &&
+        (goals_per_team[row.away_team_id][:total_goals] += row.away_goals.to_i) && (goals_per_team[row.away_team_id][:away_games_played] += 1)
       end
-      if !goals_per_team.has_key?(row[:home_team_id])
-        goals_per_team[row[:home_team_id]] = {team_name: nil,
+      if !goals_per_team.has_key?(row.home_team_id)
+        goals_per_team[row.home_team_id] = {team_name: nil,
           goals_while_away: 0,
-          goals_while_home: row[:home_goals].to_i,
-          total_goals: row[:home_goals].to_i,
+          goals_while_home: row.home_goals.to_i,
+          total_goals: row.home_goals.to_i,
           away_games_played: 0,
           home_games_played: 1,
           total_games_played: 1,
           avg_goals_per_game_away: 0,
           avg_goals_per_game_home: 0,
           avg_goals_per_game_total: 0}
-      elsif goals_per_team.has_key?(row[:home_team_id])
-        (goals_per_team[row[:home_team_id]][:goals_while_home] += row[:home_goals].to_i) && (goals_per_team[row[:home_team_id]][:total_games_played] += 1) &&
-        (goals_per_team[row[:home_team_id]][:total_goals] += row[:home_goals].to_i) && (goals_per_team[row[:home_team_id]][:home_games_played] += 1)
+      elsif goals_per_team.has_key?(row.home_team_id)
+        (goals_per_team[row.home_team_id][:goals_while_home] += row.home_goals.to_i) && (goals_per_team[row.home_team_id][:total_games_played] += 1) &&
+        (goals_per_team[row.home_team_id][:total_goals] += row.home_goals.to_i) && (goals_per_team[row.home_team_id][:home_games_played] += 1)
       end
     end
     # calculates averages and assigns team names
     @teams_data.each do |row|
       goals_per_team.each_pair do |team, stats|
-        row[:team_id] == team ? stats[:team_name] = row[:teamname] : next
+        row.team_id == team ? stats[:team_name] = row.teamname : next
         stats[:avg_goals_per_game_total] = stats[:total_goals].fdiv(stats[:total_games_played])
         stats[:avg_goals_per_game_away] = stats[:goals_while_away].fdiv(stats[:away_games_played])
         stats[:avg_goals_per_game_home] = stats[:goals_while_home].fdiv(stats[:home_games_played])
