b8dc9ef0f4cff2786b0e34b6f86446373331aafa
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-08 16:25:50 -0600

    Refactor #team_info. Hash keys return as strings instead of symbols

--- lib/team_statistics.rb
+++ lib/team_statistics.rb
@@ -5,11 +5,11 @@ module TeamStatistics
       row.team_id == team_id
     end
     team_info = {
-      team_id: team_info_full.team_id,
-      franchise_id: team_info_full.franchiseid,
-      team_name: team_info_full.teamname,
-      abbreviation: team_info_full.abbreviation,
-      link: team_info_full.link}
+      "team_id" => team_info_full.team_id,
+      "franchise_id" => team_info_full.franchiseid,
+      "team_name" => team_info_full.teamname,
+      "abbreviation" => team_info_full.abbreviation,
+      "link" => team_info_full.link}
     team_info
   end
 
