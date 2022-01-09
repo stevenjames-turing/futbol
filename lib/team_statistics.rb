0eb1f7e421c5a6f66e7ee66cc8ef8b56f191c6cf
Sam Smith <samlsmith424@gmail.com>
2022-01-08 17:19:26 -0800

    Feat: #most_goals_scored & #least_goals_scored

--- lib/team_statistics.rb
+++ lib/team_statistics.rb
@@ -15,24 +15,20 @@ module TeamStatistics
 
   def best_season(team_id)
     seasons = []
-    wins = @game_teams_data.select do |x|
-      (x.team_id == team_id) && (x.result != "LOSS") #|| (x.result == "TIE"))
-    end
+    wins = find_outcome(team_id, "WIN")
     wins.each do |win|
       @games_data.each do |game|
-        if (game.game_id == win.game_id) && (game.type == "Regular Season")
+        if (game.game_id == win.game_id) && (game.type == "Postseason")
           seasons << game.season
         end
       end
     end
-    best_season = seasons.max_by{|season| season.count(season)}
+    best_season = seasons.max_by{|season| seasons.count(season)}
   end
 
   def worst_season(team_id)
     seasons = []
-    wins = @game_teams_data.select do |x|
-      (x.team_id == team_id) && (x.result == "LOSS")
-    end
+    wins = find_outcome(team_id, "LOSS")
     wins.each do |win|
       @games_data.each do |game|
         if (game.game_id == win.game_id) && (game.type == "Regular Season")
@@ -40,6 +36,35 @@ module TeamStatistics
         end
       end
     end
-    worst_season = seasons.max_by{|season| season.count(season)}
+    worst_season = seasons.max_by{|season| seasons.count(season)}
+  end
+
+  def average_win_percentage(team_id)
+    total_wins = @game_teams_data.count do |game|
+      game.team_id == team_id && game.result == "WIN"
+    end
+    total_games = @game_teams_data.count do |game|
+      game.team_id == team_id
+    end
+    (total_wins.to_f / total_games) * 100
+  end
+
+  def most_goals_scored(team_id)
+    @game_teams_data.max_by do |game|
+      game.team_id == team_id ? game.goals : 0
+    end.goals
+  end
+
+  def fewest_goals_scored(team_id)
+    @game_teams_data.select do |game|
+      game.team_id == team_id
+    end.min_by {|team| team.goals}.goals
+  end
+
+  private
+  def find_outcome(team_id, desired_result)
+    @game_teams_data.select do |x|
+      (x.team_id == team_id) && (x.result == desired_result) #|| (x.result == "TIE"))
+    end
   end
 end
