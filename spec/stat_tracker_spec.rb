8f5e019e212ee481ada23f77678dd0802db8f687
Sam Smith <samlsmith424@gmail.com>
2022-01-10 08:33:34 -0800

    Refactor spacing

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,39 +1,49 @@
 require 'simplecov'
 SimpleCov.start
 require './lib/stat_tracker'
-# require './lib/game'
 
 RSpec.describe StatTracker do
-  include TeamStatistics
-  # before(:all) do
-  #   game_path = './data/test_games.csv'
-  #   team_path = './data/test_teams.csv'
-  #   game_teams_path = './data/test_game_teams.csv'
-  # end
-  it "exists" do
-    game_path = './data/test_games.csv'
-    team_path = './data/test_teams.csv'
-    game_teams_path = './data/test_game_teams.csv'
+  before(:all) do
+    # game_path = './data/test_games.csv'
+    # team_path = './data/test_teams.csv'
+    # game_teams_path = './data/test_game_teams.csv'
+
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
 
     locations = {
       games: game_path,
       teams: team_path,
       game_teams: game_teams_path
     }
-    stat_tracker = StatTracker.from_csv(locations)
-    expect(stat_tracker).to be_instance_of(StatTracker)
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
+
+  it "exists" do
+    # game_path = './data/test_games.csv'
+    # team_path = './data/test_teams.csv'
+    # game_teams_path = './data/test_game_teams.csv'
+    #
+    # locations = {
+    #   games: game_path,
+    #   teams: team_path,
+    #   game_teams: game_teams_path
+    # }
+    # stat_tracker = StatTracker.from_csv(locations)
+    expect(@stat_tracker).to be_instance_of(StatTracker)
   end
 
   it 'can count the number of teams in the data' do
-    game_path = './data/test_games.csv'
-    team_path = './data/test_teams.csv'
-    game_teams_path = './data/test_game_teams.csv'
-    locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path}
-    stat_tracker = StatTracker.from_csv(locations)
-    expect(stat_tracker.count_of_teams).to eq(32)
+    # game_path = './data/test_games.csv'
+    # team_path = './data/test_teams.csv'
+    # game_teams_path = './data/test_game_teams.csv'
+    # locations = {
+    #   games: game_path,
+    #   teams: team_path,
+    #   game_teams: game_teams_path}
+    # stat_tracker = StatTracker.from_csv(locations)
+    expect(@stat_tracker.count_of_teams).to eq(32)
   end
 
   it 'can create a hash with team_id as keys and a nested hash with goal data' do
@@ -120,5 +130,45 @@ RSpec.describe StatTracker do
       game_teams: game_teams_path}
       stat_tracker = StatTracker.from_csv(locations)
       expect(stat_tracker.lowest_scoring_home_team).to eq("Utah Royals FC")
+  end
+
+  it "#team_info" do
+    expected = {
+      "team_id" => "18",
+      "franchise_id" => "34",
+      "team_name" => "Minnesota United FC",
+      "abbreviation" => "MIN",
+      "link" => "/api/v1/teams/18"
+    }
+
+    expect(@stat_tracker.team_info("18")).to eq expected
+  end
+
+  it "#best_season" do
+    expect(@stat_tracker.best_season("6")).to eq "20132014"
+  end
+
+  xit "#worst_season" do #it is returing 20152016
+    expect(@stat_tracker.worst_season("6")).to eq "20142015"
+  end
+
+  it "#average_win_percentage" do
+    expect(@stat_tracker.average_win_percentage("6")).to eq 0.49
+  end
+
+  it "#most_goals_scored" do
+    expect(@stat_tracker.most_goals_scored("18")).to eq 7
+  end
+
+  it "#fewest_goals_scored" do
+    expect(@stat_tracker.fewest_goals_scored("18")).to eq 0
+  end
+
+  xit "#favorite_opponent" do
+    expect(@stat_tracker.favorite_opponent("18")).to eq "DC United"
+  end
+
+  it "#rival" do
+    expect(@stat_tracker.rival("18")).to eq("Houston Dash").or(eq("LA Galaxy"))
   end
 end
