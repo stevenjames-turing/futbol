49a77400514e48c5dd963ae344c546465e4f9cdf
Alicia Watt <alicia.watt.aw@gmail.com>
2022-01-10 10:49:26 -0700

    remove unused method

--- lib/season_stats.rb
+++ lib/season_stats.rb
@@ -1,19 +1,5 @@
 require 'csv'
 module SeasonStats
-
-  # def game_teams_by_coaches(season)
-  #   game_teams_by_coach = games_teams_in_season(season).group_by { |game_teams| game_teams.head_coach}
-  # end
-  #
-  # def average_wins_by_coach(season)
-  #   average_percent_won_by_coaches = Hash.new
-  #   game_teams_by_coaches(season).each do |coach, game_teams|
-  #     total_wins_by_coach = game_teams.find_all{ |game_team| game_team.result == 'WIN'}.count.to_f
-  #     average_percent_won_by_coaches[coach] = total_wins_by_coach / game_teams.count.to_f
-  #
-  #   end
-  #   average_percent_won_by_coaches
-  # end
   def games_played_in_season(season)
     games_by_season = @games_data.group_by {|game| game.season}
     games_in_season = games_by_season.keep_if {|key, value| key == season}
