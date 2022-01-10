e3ab9e3994bf8ed8d56c670cdb776a52bcee396c
Tyler Ross <tross020890@gmail.com>
2022-01-10 12:47:37 -0600

    Refactor: move total goals to method

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -1,14 +1,13 @@
-#Pseudo
-#methods called by stat stacker
-
-#highest_total_score : iterate through games. if sum of both scores higher than current high, set equal to current high
-
-#lowest_total_score : iterate through games. if sum of both scores lower than current low, set equal to current high
-
-#percentage_home_wins: iterate through games. If home won, home wins +=1 . return home wins / total games
-require "csv"
 module GameStatistics
-  #row[:symbol] will just be row.symbol
+
+  def total_games
+    total_games = 0.0
+    @games_data.each do |row|
+      total_games += 1
+    end
+    return total_games
+  end
+
   def highest_total_score
     most_goals = 0
     @games_data.each do |row|
@@ -21,7 +20,6 @@ module GameStatistics
   end
 
   def lowest_total_score
-
     least_goals = 10
     @games_data.each do |row|
       total_goals= (row.home_goals.to_i)+(row.away_goals.to_i)
@@ -34,44 +32,38 @@ module GameStatistics
 
   def percentage_home_wins
     home_wins = 0.0
-    total_games = 0.0
     @games_data.each do |row|
-      total_games += 1
       if ((row.home_goals)>(row.away_goals))
         home_wins += 1
       end
     end
-    percent_home_wins = home_wins/total_games
+    percent_home_wins = home_wins/self.total_games
     return percent_home_wins.round(2)
   end
 
   def percentage_visitor_wins
     away_wins = 0.0
-    total_games = 0.0
     @games_data.each do |row|
-      total_games += 1
       if ((row.home_goals)<(row.away_goals))
         away_wins += 1
       end
     end
-    percent_away_wins = away_wins/total_games
+    percent_away_wins = away_wins/self.total_games
     return percent_away_wins.round(2)
   end
 
   def percentage_ties
     ties = 0.0
-    total_games = 0.0
     @games_data.each do |row|
-      total_games += 1
       if ((row.home_goals)==(row.away_goals))
         ties += 1
       end
     end
-    percent_away_wins = ties/total_games
+    percent_away_wins = ties/self.total_games
     return percent_away_wins.round(2)
   end
 
-  def count_of_games_by_season #games:season
+  def count_of_games_by_season 
     count_of_games_by_season = {}
     @games_data.each do |row|
       count_of_games_by_season[row.season] = 0
@@ -83,13 +75,11 @@ module GameStatistics
   end
 
   def average_goals_per_game
-    total_games = 0.0
     total_goals = 0.0
     @games_data.each do |row|
-      total_games +=1
       total_goals = total_goals + (row.home_goals.to_f + row.away_goals.to_f)
     end
-    average_goals_per_game = total_goals / total_games
+    average_goals_per_game = total_goals / self.total_games
     return average_goals_per_game.round(2)
   end
 
