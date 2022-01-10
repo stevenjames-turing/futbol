07a3d492319cdfa185e3d8606e5fc1a9aaa2cddd
Alicia Watt <alicia.watt.aw@gmail.com>
2022-01-10 09:32:13 -0700

    include SeasonStats

--- lib/season_stats.rb
+++ lib/season_stats.rb
@@ -1,22 +1,37 @@
 require 'csv'
 module SeasonStats
 
-  def game_teams_by_coaches(season)
-    game_teams_by_coach = games_teams_in_season(season).group_by { |game_teams| game_teams.head_coach}
+  # def game_teams_by_coaches(season)
+  #   game_teams_by_coach = games_teams_in_season(season).group_by { |game_teams| game_teams.head_coach}
+  # end
+  #
+  # def average_wins_by_coach(season)
+  #   average_percent_won_by_coaches = Hash.new
+  #   game_teams_by_coaches(season).each do |coach, game_teams|
+  #     total_wins_by_coach = game_teams.find_all{ |game_team| game_team.result == 'WIN'}.count.to_f
+  #     average_percent_won_by_coaches[coach] = total_wins_by_coach / game_teams.count.to_f
+  #
+  #   end
+  #   average_percent_won_by_coaches
+  # end
+  def games_played_in_season(season)
+    games_by_season = @games.group_by {|game| game.season}
+    games_in_season = games_by_season.keep_if {|key, value| key == season}
+    season_game_ids = games_in_season.map {|season, games| games.map {|game| game.game_id}}.flatten
+    @game_teams.find_all{|games| season_game_ids.include?(games.game_id)}
   end
 
-  def average_wins_by_coach(season)
-    average_percent_won_by_coaches = Hash.new
-    game_teams_by_coaches(season).each do |coach, game_teams|
-      total_wins_by_coach = game_teams.find_all{ |game_team| game_team.result == 'WIN'}.count.to_f
-      average_percent_won_by_coaches[coach] = total_wins_by_coach / game_teams.count.to_f
-
-    end
-    average_percent_won_by_coaches
+  def game_stats_by_team_id(season)
+    games_played_in_season(season).group_by{|game_teams| game_teams.team_id}
   end
 
   def winningest_coach(season)
-    average_wins_by_coach(season).max_by{|coach, average_wins| average_wins}[0]
+    games_played_in_season(season)
+    games_played_by_coach = games_played_in_season(season).group_by {|game| game.head_coach}
+    number_of_games_coached = games_played_by_coach.transfrom_values {|value| value.count}
+    games_won = games_played_by_coach.transfrom_values {|value| value.select {|game| game.result == "WIN"}}.transfrom_values {|value| value.count}
+    avg = number_of_games_coached.merge(games_won){|key, games_played, games_won| games_won.to_f / games_played.to_f}
+    coach_name = avg.key(avg.values.max)
   end
 
 
@@ -48,16 +63,7 @@ module SeasonStats
           accuracy_hash[team_id] = accuracy
         end
 
-        def games_played_in_season(season)
-          games_by_season = @games.group_by {|game| game.season}
-          games_in_season = games_by_season.keep_if {|key, value| key == season}
-          season_game_ids = games_in_season.map {|season, games| games.map {|game| game.game_id}}.flatten
-          @game_teams.find_all{|games| season_game_ids.include?(games.game_id)}
-        end
 
-        def game_stats_by_team_id(season)
-          games_played_in_season(season).group_by{|game_teams| game_teams.team_id}
-        end
 
         def most_tackles(season)
           team_tackles_totals = game_stats_by_team_id(season).transfrom_values{|values| values.map{|game_teams| game_teams.tackles.to_i}.inject(:+)}
