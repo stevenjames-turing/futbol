5776409bb1617adf70e6ba49b58e01c5a5f4b594
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-10 20:56:59 -0600

    Refactor #most_tackles to use #team_info helper method

--- lib/season_stats.rb
+++ lib/season_stats.rb
@@ -34,54 +34,53 @@ module SeasonStats
     coach_name = avg.key(avg.values.max)
   end
 
-
-    def worst_coach(season)
-      average_wins_by_coach(season).min_by{|coach, average_wins| average_wins}[0]
-    end
-
-    def accuracy(game_teams)
-      total_goals = game_teams.map{|game_team| game_team.goals}.sum.to_f
-      total_shots = game_teams.map{|game_team| game_team.shots.to_i}.sum.to_f
-      accuracy = total_goals/total_shots
-    end
-
-    def most_accurate_team(season)
-      most_accurate = nil
-      best_accuracy = 0.01
-      games_teams_in_season = games_played_in_season(season)
-      games_team_by_team = games_teams_in_season.group_by{|game_team| game_team.team_id}
-      games_team_by_team.each do |team_id, game_teams|
-        team_accuracy = accuracy(game_teams)
-        if team_accuracy >= best_accuracy.to_f
-          (best_accuracy = team_accuracy) && (most_accurate = team_id)
-        end
-      end
-      team_info(most_accurate)["team_name"]
-    end
-
-    def least_accurate_team(season)
-      least_accurate = nil
-      worst_accuracy = 1.00
-      games_teams_in_season = games_played_in_season(season)
-      games_team_by_team = games_teams_in_season.group_by{|game_team| game_team.team_id}
-      games_team_by_team.each do |team_id, game_teams|
-        team_accuracy = accuracy(game_teams)
-        if team_accuracy <= worst_accuracy.to_f
-          (worst_accuracy = team_accuracy) && (least_accurate = team_id)
-        end
-      end
-      team_info(least_accurate)["team_name"]
-    end
-
-    def most_tackles(season)
-      team_tackles_totals = game_stats_by_team_id(season).transfrom_values{|values| values.map{|game_teams| game_teams.tackles.to_i}.inject(:+)}
-      team_id = team_tackles_totals.key(team_tackles_totals.values.max)
-      team_name_from_id(team_id)
-    end
-
-    def fewest_tackles(season)
-      team_tackles_totals = game_stats_by_team_id(season).transfrom_values{|values| values.map{|game_teams| game_teams.tackles.to_i}.inject(:+)}
-      team_id = team_tackles_totals.key(team_tackles_totals.values.min)
-      team_name_from_id(team_id)
-    end
+  def worst_coach(season)
+    average_wins_by_coach(season).min_by{|coach, average_wins| average_wins}[0]
   end
+
+  def accuracy(game_teams)
+    total_goals = game_teams.map{|game_team| game_team.goals}.sum.to_f
+    total_shots = game_teams.map{|game_team| game_team.shots.to_i}.sum.to_f
+    accuracy = total_goals/total_shots
+  end
+
+  def most_accurate_team(season)
+    most_accurate = nil
+    best_accuracy = 0.01
+    games_teams_in_season = games_played_in_season(season)
+    games_team_by_team = games_teams_in_season.group_by{|game_team| game_team.team_id}
+    games_team_by_team.each do |team_id, game_teams|
+      team_accuracy = accuracy(game_teams)
+      if team_accuracy >= best_accuracy.to_f
+        (best_accuracy = team_accuracy) && (most_accurate = team_id)
+      end
+    end
+    team_info(most_accurate)["team_name"]
+  end
+
+  def least_accurate_team(season)
+    least_accurate = nil
+    worst_accuracy = 1.00
+    games_teams_in_season = games_played_in_season(season)
+    games_team_by_team = games_teams_in_season.group_by{|game_team| game_team.team_id}
+    games_team_by_team.each do |team_id, game_teams|
+      team_accuracy = accuracy(game_teams)
+      if team_accuracy <= worst_accuracy.to_f
+        (worst_accuracy = team_accuracy) && (least_accurate = team_id)
+      end
+    end
+    team_info(least_accurate)["team_name"]
+  end
+
+  def most_tackles(season)
+    team_tackles_totals = game_stats_by_team_id(season).transform_values{|values| values.map{|game_teams| game_teams.tackles.to_i}.inject(:+)}
+    team_id = team_tackles_totals.key(team_tackles_totals.values.max)
+    team_info(team_id)["team_name"]
+  end
+
+  def fewest_tackles(season)
+    team_tackles_totals = game_stats_by_team_id(season).transform_values{|values| values.map{|game_teams| game_teams.tackles.to_i}.inject(:+)}
+    team_id = team_tackles_totals.key(team_tackles_totals.values.min)
+    team_info(team_id)["team_name"]
+  end
+end
