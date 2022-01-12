ede60f477e298e4cf94eef1556b5870cfb9c7c74
Alicia Watt <alicia.watt.aw@gmail.com>
2022-01-12 14:53:49 -0700

    add tests to stat_tracker

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -111,4 +111,49 @@ RSpec.describe StatTracker do
   it "#rival" do
     expect(@stat_tracker.rival("18")).to eq("Houston Dash").or(eq("LA Galaxy"))
   end
+
+  # 'SeasonStats module methods'
+  it '#games_played_in_season' do
+    expect(@stat_tracker.games_played_in_season("20122013")).to be_a(Array)
+  end
+
+  it 'game_stats_by_team_id' do
+    expect(@stat_tracker.game_stats_by_team_id("20122013")).to be_a(Hash)
+  end
+
+ describe '#winningest_coach' do
+   it 'returns the name of the Coach with the best win percentage for the season' do
+     expect(@stat_tracker.winningest_coach("20132014")).to eq("Claude Julien")
+   end
+  end
+
+  describe '#worst_coach' do
+    it 'returns the name of the Coach with the worst win percentage for the season' do
+      expect(@stat_tracker.worst_coach("20132014")).to eq("Peter Laviolette")
+    end
+  end
+
+  describe '#most_accurate_team' do
+  it 'returns the name of the Team with the best ration of shots to goals for the season' do
+    expect(@stat_tracker.most_accurate_team("20132014")).to eq("Real Salt Lake")
+    end
+  end
+
+  describe '#least_accurate_team' do
+    it 'returns the name of the Team with the most ration of shots to goals for the season' do
+      expect(@stat_tracker.least_accurate_team("20132014")).to eq("New York City FC")
+    end
+  end
+
+  describe '#most_tackles' do
+    it 'returns the name of the Team with the most tackles in the season' do
+      expect(@stat_tracker.most_tackles("20132014")).to eq("FC Cincinnati")
+    end
+  end
+
+  describe '#fewest_tackles' do
+    it 'returns the name of the Team with the most tackles in the season' do
+      expect(@stat_tracker.fewest_tackles("20132014")).to eq("Atlanta United")
+    end
+  end
 end
